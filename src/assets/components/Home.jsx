1a1e14cc5624241f49918ea1292a2968fcf2db5f
Iwona Malkinska <iwonamalkinska@Iwonas-MacBook-Air.local>
2023-03-21 09:46:47 +0000

    created home page/needs to add hero photo

--- src/assets/components/Home.jsx
+++ src/assets/components/Home.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import heroimg from '../heroimg.jpeg';
+import { MdOutlineArrowForward } from 'react-icons/md';
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div name='home' className='h-screen w-full bg-gradient-to-b from-black to-amber-800 '>
+
+      <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
+        <div>
+          <h2>I'm a Frontend Developer</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+        </div>
+        <div>
+          <button>Portfolio
+            <span> <MdOutlineArrowForward/> </span>
+              </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Home
