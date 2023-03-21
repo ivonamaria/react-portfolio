8c2435656a56d56f00429d5093eecee3eb2f96ea
Iwona Malkinska <iwonamalkinska@Iwonas-MacBook-Air.local>
2023-03-21 22:26:38 +0000

    added social links to page

--- src/assets/components/SocialLinks.jsx
+++ src/assets/components/SocialLinks.jsx
@@ -1,21 +1,77 @@
-import React from 'react'
-import {FaGithub, FaLinkedin} from 'react-icons/fa'
-import {HiOutlineMail} from 'react-icons/hi'
-import {BsFillPersonLinesFill} from 'react-icons/bs'
-
+import React from 'react';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import { HiOutlineMail } from 'react-icons/hi';
+import { BsFillPersonLinesFill } from 'react-icons/bs';
 
 const SocialLinks = () => {
-  return (
-    <div>
-        <ul>
-            <li><a href=""><>
-            LinkedIn <FaLinkedin />
-            </>
-            </a>
-            </li>
-        </ul>
-    </div>
-  )
-}
+  const links = [
+    {
+      id: 1,
+      child: (
+        <>
+          LinkedIn  <FaLinkedin size={30} />
+        </>
+      ),
+      href: 'https://www.linkedin.com/in/iwona-malkinska-90a941215/',
+      style: 'rounded-tr-md',
+    },
+    {
+      id: 2,
+      child: (
+        <>
+          GitHub  <FaGithub size={30} />
+        </>
+      ),
+      href: 'https://github.com/ivonamaria',
+    },
+    {
+      id: 3,
+      child: (
+        <>
+          Mail <HiOutlineMail size={30} />
+        </>
+      ),
+      href: 'mailto:ivonkaa@hotmail.com',
+    },
+    {
+      id: 4,
+      child: (
+        <>
+          Resume <BsFillPersonLinesFill size={30} />
+        </>
+      ),
+      href: '/resume-sample.pdf',
+      style: 'rounded-br-md',
+      download: true,
+    },
+  ];
 
-export default SocialLinks
+  return (
+    <div className='flex flex-col top-[35%] left-0 fixed'>
+      <ul>
+        {links.map(({ id, child, href, style, download }) => (
+          <li
+            key={id}
+            className={
+              'flex justify-between items-center w-40 h-14 px-4 ml-[-90px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-800' +
+              ' ' +
+              style
+            }
+          >
+            <a
+              href={href}
+              className='flex justify-center items-center px-4 w-full text-white' 
+              download={download}
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              {child}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default SocialLinks;
